9042a05ae76f9db36bcb90f22912e5653820b803
shivani kashyap <83059598+shivanikashyap1032@users.noreply.github.com>
2024-06-18 14:57:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cloudcontainers-terraform-state-1"
+    bucket         = "cloudcontainer-terraform-state"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
